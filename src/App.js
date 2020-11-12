d271a08bd671f7b4178ac55b8b940d2008600052
Sunwha <ohsw17@naver.com>
2020-11-12 15:39:08 +0900

    put html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component{
   state = {
@@ -20,9 +21,29 @@ class App extends React.Component{
   }
   render(){
     const {isLoading, movies} = this.state; // 오브젝트의 키값을 변수 이름으로 정의
-    return <div>{isLoading ? "Loading" : movies.map(movie => (
-      <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
-    ))}</div>;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie 
+                key={movie.id}
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
