ff722a39e9319be1be5f9f283a4a99ed2cb4c8bc
Sunwha <ohsw17@naver.com>
2020-11-12 16:43:46 +0900

    css

--- src/Movie.js
+++ src/Movie.js
@@ -5,7 +5,7 @@ import "./Movie.css";
 function Movie({year, title, summary, poster, genres}){
   return (
     <div className="movie">
-      <img src={poster} alt={title} title={title}/>
+      <div className="moive__thumb"><img src={poster} alt={title} title={title}/></div>
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
